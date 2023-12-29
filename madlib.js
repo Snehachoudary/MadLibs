8520522300543f09705610edcc7d97e6534da937
Snehachoudary <155088075+Snehachoudary@users.noreply.github.com>
2023-12-29 13:33:01 +0530

    Update madlib.js

--- madlib.js
+++ madlib.js
@@ -5,7 +5,7 @@ function buildMadLib()
     let character=document.getElementById("char").value;
     let adverb=document.getElementById("adv").value;
     let adjective=document.getElementById("adj").value;
-    let ans='A Person Named '+P_name+' has a '+character+' character and is very '+adjective+" and doing "+adverb;
+    let ans='One day,my friend ' + P_name +' was visiting California and ran into ' + character + '.' +  P_name + 'ran' + adverb + ' to meet '+character+'.'+' But '+character+' turned out to be '+ adjective + ' and '+P_name+' did not enjoy the meeting.';
     console.log(ans);
     document.getElementById('res').innerHTML=ans;
 
